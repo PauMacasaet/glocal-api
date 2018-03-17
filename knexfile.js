c335539aa8e877011d199b9977323893216b5574
_Pau_Macasaet <pau_macasaet15@yahoo.com>
2018-03-18 00:10:46 +0800

    pls werk

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://postgres:1Paulomac@localhost:5432/irisdb'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
-
 };
