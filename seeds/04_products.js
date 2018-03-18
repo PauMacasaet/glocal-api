346217c1d44d8fe039f18b5fd59ac50096ccd291
_Pau_Macasaet <pau_macasaet15@yahoo.com>
2018-03-18 15:31:24 +0800

    seed

---
+++ seeds/04_products.js
@@ -0,0 +1,10 @@
+const products = require('../products');
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('products').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('products').insert(products);
+    });
+};
