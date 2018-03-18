424e975cb749b5644f3a8ffa0e8ddb220773e584
_Pau_Macasaet <pau_macasaet15@yahoo.com>
2018-03-18 17:35:59 +0800

    fixfix

--- migrations/20180317184622_create-engineer.js
+++ migrations/20180317184622_create-engineer.js
@@ -85,7 +85,7 @@ exports.up = function(knex, Promise) {
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-    knex.schema.dropTable('activties'),
+    knex.schema.dropTable('activities'),
     knex.schema.dropTable('case_monitoring'),
     knex.schema.dropTable('contact_person'),
     knex.schema.dropTable('license'),
