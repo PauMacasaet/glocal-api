a2bbfdf240dcbd79c5750d8c2df0991286aa529c
_Pau_Macasaet <pau_macasaet15@yahoo.com>
2018-03-18 13:30:56 +0800

    client

--- migrations/20180317184622_create-engineer.js
+++ migrations/20180317184622_create-engineer.js
@@ -11,12 +11,20 @@ exports.up = function(knex, Promise) {
 
     knex.schema.createTable('vendor', (table) => {
       table.varchar('principal', 50).unique().primary().notNull();
+    }), 
+
+    knex.schema.createTable('client', (table) => {
+      table.varchar('accountName', 50).unique().primary().notNull();
+      table.text('contact_details', [][]).notNull();
+      table.varchar('company_address', 100).notNull();
+      table.varchar('accountManager', 50).notNull();
     })
   ]);
 };
 
 exports.down = function(knex, Promise) {
   return Promise.all([
+    knex.schema.dropTable('client'),
     knex.schema.dropTable('vendor'),
     knex.schema.dropTable('engineer')
   ]);
