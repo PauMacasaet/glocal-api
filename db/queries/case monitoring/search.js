5995e2b6c97e73eded31917b14ce27909f1bd89c
_Pau_Macasaet <pau_macasaet15@yahoo.com>
2018-03-22 09:17:03 +0800

    search start and end

--- db/queries/case monitoring/search.js
+++ db/queries/case monitoring/search.js
@@ -5,9 +5,9 @@ module.exports = {
         return knex
         .select('glocalId', 'vendorCaseId', 'dateIdCreated', 'assignedAccountManager', 'assignedSystemsEngineer', 'case_status', 'caseDescription', 'caseTitle', 'customer', 'dateRaised', 'productName', 'severity', 'systemsEngineerLead', 'vendor')
         .from('case_monitoring')
-        .where('customer', 'like', `${search}%`)
-        .orWhere('caseTitle', 'like', `${search}%`)
-        .orWhere('caseDescription', 'like', `${search}%`)
-        .orWhere('productName', 'like', `${search}%`);
+        .where('customer', 'like', `%${search}%`)
+        .orWhere('caseTitle', 'like', `%${search}%`)
+        .orWhere('caseDescription', 'like', `%${search}%`)
+        .orWhere('productName', 'like', `%${search}%`);
     }
 }
