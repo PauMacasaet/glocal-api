2dfd6babcc646cc6d7a1ba78a7630e8a6a4c965a
_Pau_Macasaet <pau_macasaet15@yahoo.com>
2018-03-23 08:00:51 +0800

    fixed imconsistent data in client

--- db/queries/case monitoring/filter.js
+++ db/queries/case monitoring/filter.js
@@ -5,12 +5,5 @@ module.exports = {
         return knex('case_monitoring')
         .select('case_monitoring.glocalId', 'case_monitoring.customer', 'case_monitoring.case_status', 'activities.assignedSystemsEngineer', 'case_monitoring.severity', 'case_monitoring.caseTitle', 'case_monitoring.productName', 'case_monitoring.dateRaised')
         .leftJoin('activities', 'activities.trackingNo', '=', 'case_monitoring.glocalId');
-        //.where('customer', filter);
-        // .orWhere('case_status', filter)
-        // .orWhere('assignedSystemsEngineer', filter)
-        // .orWhere('severity', filter)
-        // .orWhere('vendor', filter)
-        // .orWhere('productName', filter)
-        // .orWhere('dateRaised', filter);
     }
 }
