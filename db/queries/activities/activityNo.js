68a6f0478dc4eeb775c9ef103f66f877d8132b6c
_Pau_Macasaet <pau_macasaet15@yahoo.com>
2018-03-23 18:26:56 +0800

    joined activities and contact_person in activityNo route

--- db/queries/activities/activityNo.js
+++ db/queries/activities/activityNo.js
@@ -2,11 +2,17 @@ const knex = require('../../knex'); // the connection
 
 module.exports = {
     getAll() {
-        return knex('activities');
+        return knex('activities')
+        .join('contact_person', 'activities.client', '=', 'contact_person.client')
+        .select('activities.activityNo', 'activities.trackingNo AS glocalId', 'activities.productName', 'contact_person.client', 'contact_person.personName', 'activities.typeOfActivity', 'activities.purposeOfVisit', 'activities.activityPerformed', 'activities.nextActivity', 'activities.recommendations', 'activities.timeIn', 'activities.timeOuts', 'activities.assignedSystemsEngineer')
+        .orderBy('activities.activityNo', 'asc');
     },
     getOne(activityNo) {
         return knex('activities')
-        .where('activityNo', activityNo);
+        .join('contact_person', 'activities.client', '=', 'contact_person.client')
+        .select('activities.activityNo', 'activities.trackingNo AS glocalId', 'activities.productName', 'contact_person.client', 'contact_person.personName', 'activities.typeOfActivity', 'activities.purposeOfVisit', 'activities.activityPerformed', 'activities.nextActivity', 'activities.recommendations', 'activities.timeIn', 'activities.timeOuts', 'activities.assignedSystemsEngineer')
+        .where('activityNo', activityNo)
+        .orderBy('activities.activityNo', 'asc');
     },
     create(activity) {
         return knex('activities').insert(activity, '*');
