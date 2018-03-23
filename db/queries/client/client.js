9be5a24d2e38fbecbcd2d3e1b6032dc9c7e87f9a
_Pau_Macasaet <pau_macasaet15@yahoo.com>
2018-03-23 17:32:41 +0800

    mult values sli

--- db/queries/client/client.js
+++ db/queries/client/client.js
@@ -3,11 +3,11 @@ const knex = require('../../knex'); // the connection
 module.exports = {
     getAll() {
         return knex('client')
-        .select('accountName', knex.raw('contact_details[0:2][1:1] AS Customer_Name, contact_details[2:2][2] AS Email, contact_details[3:3][3] AS Contact_Number'), 'company_address', 'accountManager');
+        .select('accountName', knex.raw('contact_details[0:2][2] AS Customer_Name, contact_details[2:2][2] AS Email, contact_details[3:3][3] AS Contact_Number'), 'company_address', 'accountManager');
     },
     getOne(accountName) {
         return knex('client')
-        .select('accountName', knex.raw('contact_details[0:1][1:1] AS Customer_Name, contact_details[2:2][2] AS Email, contact_details[3:3][3] AS Contact_Number'), 'company_address', 'accountManager')
+        .select('accountName', knex.raw('contact_details[0:2][2] AS Customer_Name, contact_details[2:2][2] AS Email, contact_details[3:3][3] AS Contact_Number'), 'company_address', 'accountManager')
         .where('accountName', accountName);
     }, 
     create(client) {
