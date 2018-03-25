0ca283d4692d9354a807b46dc4098006c38e7ced
_Pau_Macasaet <pau_macasaet15@yahoo.com>
2018-03-25 19:11:39 +0800

    added seLead in filters and search

--- db/queries/case monitoring/filter.js
+++ db/queries/case monitoring/filter.js
@@ -3,7 +3,7 @@ const knex = require('../../knex'); // the connection
 module.exports = {
     getOne(filter) { // join w activities
         return knex('case_monitoring')
-        .select('case_monitoring.glocalId', 'case_monitoring.customer', 'case_monitoring.case_status', 'activities.assignedSystemsEngineer', 'case_monitoring.severity', 'case_monitoring.caseTitle', 'case_monitoring.productName', 'case_monitoring.dateRaised')
+        .select('case_monitoring.glocalId', 'case_monitoring.customer', 'case_monitoring.case_status', 'case_monitoring.systemsEngineerLead', 'activities.assignedSystemsEngineer', 'case_monitoring.severity', 'case_monitoring.caseTitle', 'case_monitoring.productName', 'case_monitoring.dateRaised')
         .leftJoin('activities', 'activities.trackingNo', '=', 'case_monitoring.glocalId');
     }
 }
