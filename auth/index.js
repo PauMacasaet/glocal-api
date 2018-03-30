9e379879de1fe8b8bdba5f617e6b02762cea05b3
_Pau_Macasaet <pau_macasaet15@yahoo.com>
2018-03-30 22:14:01 +0800

    issecure

--- auth/index.js
+++ auth/index.js
@@ -37,7 +37,7 @@ function setUserIdCookie (req, res, id) {
     const isSecure = req.app.get('env') != 'development';
     res.cookie('user_id', id, {
         httpOnly: true,
-        secure: false,
+        secure: isSecure,
         signed: true
     });
 }
