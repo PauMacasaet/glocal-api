d6485ca3246a7cf422b3d1ba531d02b5cd740e44
_Pau_Macasaet <pau_macasaet15@yahoo.com>
2018-04-06 06:05:37 +0800

    fixed cors origin

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser('secret'));
 app.use(cors({
-  origin: 'https://carbon-app-kpnqizpvkm.now.sh/login', 
+  origin: 'https://carbon-app-kpnqjzpvkm.now.sh/login', 
   credentials: true
 }));
 
