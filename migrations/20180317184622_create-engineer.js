38be91075d3731e8411093e8e25ff7aa532a6f24
_Pau_Macasaet <pau_macasaet15@yahoo.com>
2018-04-07 14:03:40 +0800

    changed varchar address

--- migrations/20180317184622_create-engineer.js
+++ migrations/20180317184622_create-engineer.js
@@ -26,7 +26,7 @@ exports.up = function(knex, Promise) {
     knex.schema.createTable('client', (table) => {
       table.varchar('accountName', 50).unique().primary().notNull();
       table.specificType('contact_details', 'text[]').notNull();
-      table.varchar('company_address', 100).notNull();
+      table.varchar('company_address', 255).notNull();
       table.varchar('accountManager', 50).references('fullName').inTable('user').notNull().onUpdate('cascade');
     }),
 
