b460cc7d722f478dd014573fd4638a5e7b546015
_Pau_Macasaet <pau_macasaet15@yahoo.com>
2018-04-13 19:07:12 +0800

    accountmanager

--- db/queries/client/accountmanager.js
+++ db/queries/client/accountmanager.js
@@ -2,9 +2,22 @@ const knex = require('../../knex'); // the connection
 
 module.exports = {
     getAll() {
-        return knex.select('accountName', 'accountManager').from('client');
+        return knex('client')
+            .select(
+                'accountName', 
+                knex.raw('contact_details[0:2][5] AS Customer_Name, contact_details[2:2][5] AS Email, contact_details[3:3][5] AS Contact_Number'), 
+                'company_address', 
+                'accountManager'
+            );
     },
     getOne(accountManager) {
-        return knex('client').where('accountManager', accountManager);
+        return knex('client')
+            .select(
+                'accountName', 
+                knex.raw('contact_details[0:2][5] AS Customer_Name, contact_details[2:2][5] AS Email, contact_details[3:3][5] AS Contact_Number'), 
+                'company_address', 
+                'accountManager'
+            )
+            .where('accountManager', accountManager);
     }
 }
