c7a458e80961d6e163fc81d352bd24042f85bef6
_Pau_Macasaet <pau_macasaet15@yahoo.com>
2018-04-15 16:14:23 +0800

    active only

--- db/queries/license/license.js
+++ db/queries/license/license.js
@@ -97,10 +97,10 @@ module.exports = {
         //SEARCH
         if (query.q) {
             knexQuery.where('license.client', 'ILIKE', `%${query.q}%`)
-                .where('license.vendor', 'ILIKE', `%${query.q}%`)
-                .where('license.productName', 'ILIKE', `%${query.q}%`)
-                .where('license.particulars', 'ILIKE', `%${query.q}%`)
-                .where('client.accountManager', 'ILIKE', `%${query.q}%`);
+                .orWhere('license.vendor', 'ILIKE', `%${query.q}%`)
+                .orWhere('license.productName', 'ILIKE', `%${query.q}%`)
+                .orWhere('license.particulars', 'ILIKE', `%${query.q}%`)
+                .orWhere('client.accountManager', 'ILIKE', `%${query.q}%`);
         }
 
         // SORT
