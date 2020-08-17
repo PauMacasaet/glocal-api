a5a35fcd6a69aa0eb4fc0761d759b2dececd11d2
paumacasaet <pmacasaet95@gmail.com>
2020-08-17 18:57:19 +0800

    change cors URL

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser('secret'));
 app.use(cors({
-  origin: 'https://iris-glocal.now.sh',  
+  origin: 'https://iris-glocal.netlify.app',  
   //origin: 'http://localhost:8000',
   credentials: true
 }));
